b4ead37d5a46edc3b0459bd3ff7c935997a7fd4f
Antonio Gisondi <harlem88.ant@gmail.com>
2022-07-27 17:26:49 +0200

    Add send_data function to proto service
    Allows to publish a message on Astarte with send_data function,
    at this moment only Double and Int32 are supported, but this is
    only example for trying to publish data on Astarte through the MessageHub.
    Inject node-id and node-name to all node request in order to check which node
    is trying to do something.
    Rename HubMessage to NodeMessage.

--- src/hub.rs
+++ src/hub.rs
@@ -8,27 +8,30 @@ use structopt::StructOpt;
 use tokio::sync::mpsc::Sender;
 use tokio::sync::{mpsc, Mutex};
 use tokio_stream::wrappers::ReceiverStream;
+use tonic::metadata::{AsciiMetadataValue, KeyAndValueRef, MetadataValue};
 use tonic::transport::Server;
 use tonic::{Request, Response, Status};
 
 use msg_hub::msg_hub_server::{MsgHub, MsgHubServer};
-use msg_hub::{Device, HubData};
+use msg_hub::{NodeData, NodeIntrospection};
 
 use crate::msg_hub::AstarteSdkType;
-use crate::msg_hub::HubMessage;
+use crate::msg_hub::NodeMessage;
 
 pub mod msg_hub {
     tonic::include_proto!("msghub");
 }
 
 struct Node {
-    node_channel: Sender<Result<HubMessage, Status>>,
-    device: Device,
+    id: String,
+    name: String,
+    introspection: NodeIntrospection,
+    node_channel: Sender<Result<NodeMessage, Status>>,
 }
 
 pub struct MsgHubService {
-    nodes: Arc<Mutex<HashMap<i32, Node>>>,
-    deviceSDK: AstarteSdk,
+    nodes: Arc<Mutex<HashMap<String, Node>>>,
+    device_sdk: AstarteSdk,
 }
 
 #[derive(Debug, StructOpt)]
@@ -49,29 +52,97 @@ struct Cli {
 
 #[tonic::async_trait]
 impl MsgHub for MsgHubService {
-    type AttachStream = ReceiverStream<Result<HubMessage, Status>>;
+    type AttachStream = ReceiverStream<Result<NodeMessage, Status>>;
 
     async fn attach(
         &self,
-        request: Request<Device>,
+        request: Request<NodeIntrospection>,
     ) -> Result<Response<Self::AttachStream>, Status> {
         println!("attach => {:?}", request);
+
+        let node_id = request
+            .metadata()
+            .get("node-id")
+            .ok_or_else(|| AsciiMetadataValue::try_from(""))
+            .unwrap()
+            .to_str()
+            .unwrap();
+        let node_name = request
+            .metadata()
+            .get("node-name")
+            .ok_or_else(|| AsciiMetadataValue::try_from(""))
+            .unwrap()
+            .to_str()
+            .unwrap();
 
         let (tx, rx) = mpsc::channel(4);
 
         let node = Node {
+            id: node_id.to_owned(),
+            name: node_name.to_owned(),
+            introspection: request.into_inner(),
             node_channel: tx,
-            device: request.into_inner(),
         };
 
         let mut nodes = self.nodes.lock().await;
-        nodes.entry(node.device.id).or_insert(node);
+        nodes.insert(node.id.to_owned(), node);
 
         Ok(Response::new(ReceiverStream::new(rx)))
     }
 
-    async fn detach(&self, request: Request<Device>) -> Result<Response<HubData>, Status> {
-        Ok(Response::new(HubData {
+    async fn send_data(&self, request: Request<NodeMessage>) -> Result<Response<NodeData>, Status> {
+        let metadata = request.metadata().clone();
+        let node_message = request.into_inner();
+
+        let node_id = metadata
+            .get("node-id")
+            .ok_or_else(|| AsciiMetadataValue::try_from(""))
+            .unwrap()
+            .to_str()
+            .unwrap();
+        let node_name = metadata
+            .get("node-name")
+            .ok_or_else(|| AsciiMetadataValue::try_from(""))
+            .unwrap()
+            .to_str()
+            .unwrap();
+
+        let node_guard = self.nodes.lock().await;
+        let can_publish = node_guard
+            .get(node_id)
+            .map(|node| {
+                node.introspection
+                    .interfaces
+                    .iter()
+                    .filter(|iface| iface.name == node_message.interface)
+                    .count()
+                    > 0
+            })
+            .is_some();
+
+        if can_publish {
+            let astarte_type: AstarteType = node_message.data.unwrap().try_into().unwrap();
+            match node_message.aggregation_type {
+                0 => {
+                    self.device_sdk
+                        .send(&node_message.interface, &node_message.path, astarte_type)
+                        .await
+                        .unwrap();
+                }
+                _ => {}
+            }
+        }
+
+        Ok(Response::new(NodeData {
+            message: "".to_owned(),
+        }))
+    }
+
+    async fn detach(
+        &self,
+        request: Request<NodeIntrospection>,
+    ) -> Result<Response<NodeData>, Status> {
+        Ok(Response::new(NodeData {
             message: "".to_owned(),
         }))
     }
@@ -88,7 +159,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let addr = "[::1]:10000".parse().unwrap();
 
-    println!("Message Hub listening on: {}", addr);
+    println!("Message Node listening on: {}", addr);
 
     let sdk_options = AstarteOptions::new(&realm, &device_id, &credentials_secret, &pairing_url)
         .interface_directory("./interfaces")?
@@ -100,7 +171,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let msg_hub = MsgHubService {
         nodes: Arc::new(Mutex::new(HashMap::new())),
-        deviceSDK: device_clone,
+        device_sdk: device_clone,
     };
 
     let nodes = msg_hub.nodes.clone();
@@ -113,7 +184,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     println!("incoming: {:?}", data);
 
                     if let astarte_sdk::Aggregation::Individual(var) = data.data {
-                        let hub_message = msg_hub::HubMessage {
+                        let node_message = msg_hub::NodeMessage {
                             interface: data.interface.to_owned(),
                             path: data.path.to_owned(),
                             aggregation_type: msg_hub::AstAggregation::Individual.into(),
@@ -124,7 +195,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         let node_selected: Vec<&Node> = node_guard
                             .iter()
                             .filter(|(id, node)| {
-                                node.device
+                                node.introspection
                                     .interfaces
                                     .iter()
                                     .filter(|iface| iface.name == data.interface)
@@ -135,7 +206,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                             .collect();
                         for node in node_selected {
                             println!(" Node Found");
-                            let result = node.node_channel.send(Ok(hub_message.clone())).await;
+                            let result = node.node_channel.send(Ok(node_message.clone())).await;
                             if result.is_err() {
                                 println!(" Channel closed");
                             }
@@ -169,5 +240,23 @@ impl TryFrom<AstarteType> for AstarteSdkType {
         Ok(msg_hub::AstarteSdkType {
             one_of_astarte_type: Some(one_of_value?),
         })
+    }
+}
+
+impl TryFrom<AstarteSdkType> for AstarteType {
+    type Error = String;
+
+    fn try_from(value: AstarteSdkType) -> Result<Self, Self::Error> {
+        if let Some(msg_hub::astarte_sdk_type::OneOfAstarteType::Double(dvalue)) =
+            value.one_of_astarte_type
+        {
+            Ok(AstarteType::Double(dvalue))
+        } else if let Some(msg_hub::astarte_sdk_type::OneOfAstarteType::Int32(dvalue)) =
+            value.one_of_astarte_type
+        {
+            Ok(AstarteType::Integer(dvalue))
+        } else {
+            Err("Can't convert to astarte".to_owned())
+        }
     }
 }
--- src/hub_client.rs
+++ src/hub_client.rs
@@ -1,3 +1,4 @@
+use astarte_sdk::AstarteSdk;
 use std::error::Error;
 use std::sync::Arc;
 use std::time::Duration;
@@ -7,29 +8,40 @@ use futures::stream;
 use rand::rngs::ThreadRng;
 use rand::Rng;
 use tokio::time;
-use tonic::transport::Channel;
-use tonic::Request;
+use tonic::codegen::InterceptedService;
+use tonic::metadata::MetadataValue;
+use tonic::service::Interceptor;
+use tonic::transport::{Channel, Endpoint};
+use tonic::{Request, Status};
 
 use msg_hub::msg_hub_client::MsgHubClient;
-use msg_hub::{Device, HubData, Interface};
+use msg_hub::{Interface, NodeData, NodeIntrospection};
 
 pub mod msg_hub {
     tonic::include_proto!("msghub");
 }
 
-async fn run_attach(client: &mut MsgHubClient<Channel>) -> Result<(), Box<dyn Error>> {
-    let interfaces = vec![Interface {
-        name: "io.demo.ServerProperties".to_owned(),
-        minor: 0,
-        major: 2,
-    }];
-    let device = Device {
-        id: 123,
-        name: "device123".to_owned(),
-        interfaces,
-    };
+type HubClient = MsgHubClient<InterceptedService<Channel, MyInterceptor>>;
 
-    let mut stream = client.attach(Request::new(device)).await?.into_inner();
+async fn run_attach(client: &mut HubClient) -> Result<(), Box<dyn Error>> {
+    let interfaces = vec![
+        Interface {
+            name: "io.demo.ServerProperties".to_owned(),
+            minor: 0,
+            major: 2,
+        },
+        Interface {
+            name: "org.astarteplatform.esp32.DeviceDatastream".to_owned(),
+            minor: 0,
+            major: 2,
+        },
+    ];
+    let node_introspection = NodeIntrospection { interfaces };
+
+    let mut stream = client
+        .attach(Request::new(node_introspection))
+        .await?
+        .into_inner();
 
     while let Some(hub_msg) = stream.message().await? {
         println!("Msg = {:?}", hub_msg);
@@ -38,21 +50,67 @@ async fn run_attach(client: &mut MsgHubClient<Channel>) -> Result<(), Box<dyn Er
     Ok(())
 }
 
+async fn send_int(client: &mut HubClient) -> Result<(), Box<dyn Error>> {
+    use crate::msg_hub::{astarte_sdk_type::OneOfAstarteType, AstarteSdkType};
+    use std::time::{SystemTime, UNIX_EPOCH};
+
+    let node_message = msg_hub::NodeMessage {
+        interface: "org.astarteplatform.esp32.DeviceDatastream".to_owned(),
+        path: "/uptimeSeconds".to_owned(),
+        aggregation_type: msg_hub::AstAggregation::Individual.into(),
+        data: Some(AstarteSdkType {
+            one_of_astarte_type: Some(OneOfAstarteType::Int32(
+                SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap()
+                    .as_secs() as i32,
+            )),
+        }),
+    };
+
+    client.send_data(Request::new(node_message)).await?;
+
+    Ok(())
+}
+
+struct MyInterceptor;
+
+impl Interceptor for MyInterceptor {
+    fn call(&mut self, mut request: Request<()>) -> Result<Request<()>, Status> {
+        let node_id: MetadataValue<_> = "12345fag".parse().unwrap();
+        let node_name: MetadataValue<_> = "node-1".parse().unwrap();
+
+        request.metadata_mut().insert("node-id", node_id.clone());
+        request
+            .metadata_mut()
+            .insert("node-name", node_name.clone());
+
+        Ok(request)
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = MsgHubClient::connect("http://[::1]:10000").await?;
+    let channel = Endpoint::from_static("http://[::1]:10000")
+        .connect()
+        .await?;
+
+    let channel_clone = channel.clone();
+    let mut client = MsgHubClient::with_interceptor(channel, MyInterceptor);
+    let mut client_clone = MsgHubClient::with_interceptor(channel_clone, MyInterceptor);
 
     println!("\n*** ATTACH ***");
+
+    tokio::spawn(async move {
+        loop {
+            use tokio::time::{sleep, Duration};
+            send_int(&mut client_clone).await.unwrap();
+            println!("\n*** SEND ***");
+            sleep(Duration::from_secs(10)).await;
+        }
+    });
+
     run_attach(&mut client).await?;
 
     Ok(())
 }
-//
-// fn random_point(rng: &mut ThreadRng) -> Point {
-//     let latitude = (rng.gen_range(0..180) - 90) * 10_000_000;
-//     let longitude = (rng.gen_range(0..360) - 180) * 10_000_000;
-//     Point {
-//         latitude,
-//         longitude,
-//     }
-// }
